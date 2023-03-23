2d767c8ca0abe5f889288ab147f6ad4ca221a1fb
air <kobychev00@bk.ru>
2023-03-23 22:01:09 +0300

    memory111

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,12 @@
 public class Main {
-    public static void main(String[] args) {
 
-        System.out.println("Hello worleeed");
+    public static void changeValue(int value) {
+        value = 22;
+        System.out.println("Измененное значение = " + value);
+    }
+    public static void main(String[] args) {
+        int value = 33;
+        System.out.println("Изначальное значение value: " + value);
+        changeValue(value);
     }
 }
