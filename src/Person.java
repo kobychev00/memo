cd90f4dec4dcf99691a1f405b52d619d7be09585
air <kobychev00@bk.ru>
2023-03-28 21:46:37 +0300

    memory is ready to check1 eptsa

--- src/Person.java
+++ src/Person.java
@@ -7,6 +7,22 @@ public class Person {
         this.surname = surname;
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
     @Override
     public String toString() {
         return name + " " + surname;
